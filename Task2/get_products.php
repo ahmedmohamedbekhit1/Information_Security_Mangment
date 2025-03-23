4a8ed9f1ebc923e45c59c6f19c3b765145707f97
Ahmed Mohamed Bekhit <151550843+ahmedmohamedbekhit1@users.noreply.github.com>
2025-03-23 23:31:27 +0200

    Update get_products.php

--- Task2/get_products.php
+++ Task2/get_products.php
@@ -1,16 +1,14 @@
 <?php
-require 'jwt_middleware.php'; // Include the JWT middleware
+require 'jwt_middleware.php'; 
 require 'db_connection.php';
 header('Content-Type: application/json');
-
-// Fetch all products from the database
 try {
     $stmt = $conn->query("SELECT * FROM Products");
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     echo json_encode($products);
 } catch (PDOException $e) {
-    http_response_code(500); // Internal Server Error
+    http_response_code(500);
     echo json_encode(["message" => "Database error: " . $e->getMessage()]);
 }
 ?>
